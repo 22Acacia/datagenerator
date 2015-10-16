7ff23281b245dd8a8b9b71121c59d862d7390b25
pat <pat@22acacia.com>
2015-10-15 21:00:10 -0700

    create compiles.  will come back in a bit to try and execute it

--- resource_dataflow.go
+++ resource_dataflow.go
@@ -5,12 +5,13 @@ import (
 	"fmt"
 	"log"
 	"bytes"
+	"regexp"
+	"strings"
 	"os/exec"
 	"io/ioutil"
 	"encoding/json"
 
 	"github.com/hashicorp/terraform/helper/schema"
-	"google.golang.org/api/googleapi"
 )
 
 func resourceDataFlow() *schema.Resource {
@@ -46,8 +47,8 @@ func resourceDataFlow() *schema.Resource {
 				Required: true,
 			},
 
-			"jobid": &schema.Schema{
-				Type:     schema.TypeString,
+			"jobids": &schema.Schema{
+				Type:     schema.TypeList,
 				Computed: true,
 			},
 		},
@@ -63,13 +64,20 @@ type accountFile struct {
 	ClientId     string `json:"client_id"`
 }
 
+func parseJSON(result interface{}, contents string) error {
+	r := strings.NewReader(contents)
+	dec := json.NewDecoder(r)
+
+	return dec.Decode(result)
+}
+
 //  return pointer to a file that contains account information
 func setAccountFile(contents string) (string, error) {
-	if accountFile != "" {
+	if contents != "" {
 		var account accountFile
-		if err := parseJSON(&accont, contents); err == nil {
+		if err := parseJSON(&account, contents); err == nil {
 			//  raw account info, write out to a file
-			tmpfile, err := ioutil.TempFile()
+			tmpfile, err := ioutil.TempFile("","")
 			if err != nil {
 				return "", err
 			}
@@ -78,7 +86,7 @@ func setAccountFile(contents string) (string, error) {
 				return "", err
 			}
 			tmpfile.Close()
-			return tempfile.Name(), nil
+			return tmpfile.Name(), nil
 		} else {
 			return contents, nil
 		}
@@ -94,11 +102,11 @@ func cleanupTempAccountFile(rawAccountFile, account_file string) {
 
 //  init function will make sure that gcloud cli is installed,
 //  authorized and that dataflow commands are available
-func init(config *Config) error {
+func init_gcloud(config *Config) error {
 	//  check that gcloud is installed
 	_, err := exec.LookPath("gcloud")
 	if err != nil {
-		log.Error("gcloud cli is not installed.  Please install and try again")
+		log.Println("gcloud cli is not installed.  Please install and try again")
 		return err
 	}
 
@@ -108,31 +116,31 @@ func init(config *Config) error {
 	if err != nil {
 		return err
 	}
-	auth_cmd := exec.Cmd("gcloud", "auth", "activate-service-account", "--key-file", account_file)
+	auth_cmd := exec.Command("gcloud", "auth", "activate-service-account", "--key-file", account_file)
 	var stdout, stderr bytes.Buffer
 	auth_cmd.Stdout = &stdout
 	auth_cmd.Stderr = &stderr
 	err = auth_cmd.Run()
 	if err != nil {
-		log.Error("Dataflow auth failed with error: %q", stdout.String())
+		log.Println("Dataflow auth failed with error: %q", stdout.String())
 		return err 
 	}
 	
 	// verify that datacloud functions are installed
 	//  this will need to be updated when they come out of alpha
-	datacloud_cmd := exec.Cmd("gcloud", "alpha", "dataflow" , "-h")
+	datacloud_cmd := exec.Command("gcloud", "alpha", "dataflow" , "-h")
 	err = datacloud_cmd.Run()
 	if err != nil {
-		log.Error("gcloud dataflow commands not installed.")
+		log.Println("gcloud dataflow commands not installed.")
 		return err
 	}
 
 	return nil
 }
 
-func resourceDataFlowCreate(d *schema.ResourceData, meta interface{}) error {
+func resourceDataflowCreate(d *schema.ResourceData, meta interface{}) error {
 	config := meta.(*Config)
-        err := init(config)
+        err := init_gcloud(config)
 	if err != nil {
 		return err
 	}
@@ -140,57 +148,31 @@ func resourceDataFlowCreate(d *schema.ResourceData, meta interface{}) error {
 	//  at this point we have verified that our command line jankiness is going to work
 	//  get to it
 	//  I'm assuming, possibly foolishly, that java is installed on this system
-	create_dataflow_cmd := exec.Cmd("java", "-cp", d.Get("jarfile"), d.Get("class"), "--project="+d.Get("project"), "--stagingLocation="+d.Get("staging_bucket"), "--jobName="+d.Get("name"))
+	create_dataflow_cmd := exec.Command("java", "-cp", d.Get("jarfile").(string), d.Get("class").(string), "--project="+d.Get("project").(string), "--stagingLocation="+d.Get("staging_bucket").(string), "--jobName="+d.Get("name").(string))
 	var stdout, stderr bytes.Buffer
 	create_dataflow_cmd.Stdout = &stdout
 	create_dataflow_cmd.Stderr = &stderr
-	err := create_dataflow_cmd.Run()
+	err = create_dataflow_cmd.Run()
 	if err != nil {
 		return fmt.Errorf("Error submitting dataflow job: %q", stderr.String())
 	}
 	
 	//  job successfully submitted, now get the job id
-	
-
-	// Build the address parameter
-	addr := &compute.Address{Name: d.Get("name").(string)}
-	op, err := config.clientCompute.Addresses.Insert(
-		config.Project, region, addr).Do()
-	if err != nil {
-		return fmt.Errorf("Error creating address: %s", err)
+	//  SAMPLE:  Dataflow SDK version: 1.1.1-SNAPSHOT\nSubmitted job: 2015-10-15_20_14_14-4027401809971669184\nDataflow SDK version: 1.1.1-SNAPSHOT\nSubmitted job: 2015-10-15_20_14_15-9518719588918833764\n
+	jobidRe := regexp.MustCompile("Submitted job: ([0-9-_]+)\n")
+	jobidmatches := jobidRe.FindAllStringSubmatch(stdout.String(), -1)
+	jobids := make([]string, 0)
+	for _, match := range jobidmatches {
+		jobids = append(jobids, match[1])
 	}
+	d.Set("jobids", jobids)
 
-	// It probably maybe worked, so store the ID now
-	d.SetId(addr.Name)
-
-	err = computeOperationWaitRegion(config, op, region, "Creating Address")
-	if err != nil {
-		return err
-	}
-
-	return resourceComputeAddressRead(d, meta)
+	return nil
 }
 
 func resourceDataflowRead(d *schema.ResourceData, meta interface{}) error {
 	config := meta.(*Config)
 
-	region := getOptionalRegion(d, config)
-
-	addr, err := config.clientCompute.Addresses.Get(
-		config.Project, region, d.Id()).Do()
-	if err != nil {
-		if gerr, ok := err.(*googleapi.Error); ok && gerr.Code == 404 {
-			// The resource doesn't exist anymore
-			d.SetId("")
-
-			return nil
-		}
-
-		return fmt.Errorf("Error reading address: %s", err)
-	}
-
-	d.Set("address", addr.Address)
-	d.Set("self_link", addr.SelfLink)
 
 	return nil
 }
@@ -198,19 +180,8 @@ func resourceDataflowRead(d *schema.ResourceData, meta interface{}) error {
 func resourceDataflowDelete(d *schema.ResourceData, meta interface{}) error {
 	config := meta.(*Config)
 
-	region := getOptionalRegion(d, config)
 	// Delete the address
 	log.Printf("[DEBUG] address delete request")
-	op, err := config.clientCompute.Addresses.Delete(
-		config.Project, region, d.Id()).Do()
-	if err != nil {
-		return fmt.Errorf("Error deleting address: %s", err)
-	}
-
-	err = computeOperationWaitRegion(config, op, region, "Deleting Address")
-	if err != nil {
-		return err
-	}
 
 	d.SetId("")
 	return nil
